cd9b74bf8b20d029cba1439fd28a3e9921494175
Nour Rahal-Arabi <rahalarn@oregonstate.edu>
2021-03-05 15:53:47 -0800

    Updated implementation to check all leap year conditions

--- leapYear.py
+++ leapYear.py
@@ -1,4 +1,6 @@
 def leap_year(num):
+   if (int(num) % 4 == 0) and (int(num) % 100 == 0) and (int(num) % 400 == 0):
+      return True
    if (int(num) % 4 == 0) and (int(num) % 100 != 0):
       return True
    else:
