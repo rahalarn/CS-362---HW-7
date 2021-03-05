835c0f542148b84ff3733650d218522576a70a9e
Nour Rahal-Arabi <rahalarn@oregonstate.edu>
2021-03-05 15:51:39 -0800

    Added second test, now checking for both leap year conditions

--- test_leapYear.py
+++ test_leapYear.py
@@ -5,6 +5,9 @@ class TestLeap(unittest.TestCase):
     def test1(self):
         self.assertIs(leapYear.leap_year(2021), False)
 
+    def test2(self):
+        self.assertIs(leapYear.leap_year(400), True)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2)
