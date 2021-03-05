d6b42dda859fe7b1306c381eba40a0bcdeb648c1
Nour Rahal-Arabi <rahalarn@oregonstate.edu>
2021-03-05 15:33:03 -0800

    Updated implementation for second test - checks for Fizz, Buzz, and FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,12 +5,26 @@ def fizz():
       if num % 3 == 0 and num % 5 == 0:    
          res += (" " + str(num))
       elif num % 3 == 0:
-          res += " Fizz"
+         res += " Fizz"
       elif num % 5 == 0:
-          res += " Buzz"
+         res += " Buzz"
       else:
-          res += (" " + str(num))
+         res += (" " + str(num))
       num += 1
    return res
 
-fizz()   
+def fizzbuzz():
+   num = 2
+   res = "1"
+   for i in range(99):
+      if num % 3 == 0 and num % 5 == 0:    
+         res += " FizzBuzz"
+      elif num % 3 == 0:
+         res += " Fizz"
+      elif num % 5 == 0:
+         res += " Buzz"
+      else:
+         res += (" " + str(num))
+      num += 1
+   return res
+
